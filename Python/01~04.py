de1826906c02bc66176d574c10388ee3f57d15ed
Jinwoo Park <norang2810@naver.com>
2025-05-23 22:13:29 +0900

    5회차 강의 내용

--- Python/01~04.py
+++ Python/01~04.py
@@ -4,3 +4,17 @@ print("VSCode에서 Python 실행 성공!")
 num1 = 10
 print(num1)
 
+# 논리 연산자
+a=3
+b=3
+print(a>b)
+print(a>=b)
+print(a<b)
+print(a<=b)
+print(a==b)
+print("-------------")
+print (not a==b)
+
+print("-------------")
+print(3>5 and 10<20)
+
