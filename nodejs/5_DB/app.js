c4635e393a74c7de2efc985afacc1f4f9c6225d8
Jinwoo Park <norang2810@naver.com>
2025-07-18 17:37:18 +0900

    react -> node -> DB [Login]flow study

--- nodejs/5_DB/app.js
+++ nodejs/5_DB/app.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express();
 const mainRouter = require('./routes/mainRouter');
 const userRouter = require('./routes/userRouter');
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.static(__dirname+'/public')) //정적파일 미들웨어 
 
 // post방식 시 데이터 변환을 위한 미들웨어
 app.use(express.urlencoded({extends : true}))
-
+app.use(express.json())  // <-axios로 통신하고 data 키값을 가져올때 axios는 애초에 객체로 가져온다는건데
+// 그러기위해선 이 한줄을 명시해줘야한다.
 app.use('/',mainRouter)
 app.use('/user',userRouter)
 
--- nodejs/5_DB/routes/userRouter.js
+++ nodejs/5_DB/routes/userRouter.js
@@ -4,20 +4,24 @@ const router = express.Router()
 const conn = require('../config/db')
 
 // 회원가입의 *기능* 라우팅
+// http://localhost:3001/user/join
 router.post('/join', (req,res)=>{
+    console.log('접근 확인')
     console.log('user join',req.body)
-    let {id,pw,userName,mbti,song} =req.body    //비구조화 할당 개념.
+    let {id,pw,name,mbti,song} =req.body    //비구조화 할당 개념.
     console.log('ID :',id)
 
     // 사용자의 정보를 DB에 넣어야한다! ->MySQL 사용
     
     let sql = "INSERT INTO NODEJS_MEMBER VALUES (?,?,?,?,?)"
-    conn.query(sql,[id,pw,userName,mbti,song], (err,rows)=>{
+    conn.query(sql,[id,pw,name,mbti,song], (err,rows)=>{
         console.log('rows',rows)
-        if(rows){
-            res.redirect('/')
+        if(rows.affectedRows>0){
+            // res.redirect('/')
+            res.json(1) //<-응답을 json형태로 함.
         }else{
-            res.send('<script>alert("회원가입 실패!");window.location.href="http://localhost:3001/login"</script>')
+            res.json(0)
+            // res.send('<script>alert("회원가입 실패!");window.location.href="http://localhost:3001/login"</script>')
         }
 
     })
